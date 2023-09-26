691f02dfd6ef5277a68ed1efb020d22bde4ec7f1
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-09-26 11:16:58 -0400

    initial commit

---
+++ Main.ino
@@ -0,0 +1,18 @@
+// Created By Jakub Malhotra
+// Created On September 2023
+// Blink with external LED
+
+int pin5 = 5;
+
+void setup()
+{
+  pinMode(pin5, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(pin5, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(pin5, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
